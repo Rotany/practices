23ef7f0a511c160d2e6d45ef58b3acd5688f0fd8
Rotany <Rotany38@gmail.com>
2023-07-08 23:30:38 +0200

    utilizando pandas

--- code/filter.py
+++ code/filter.py
@@ -1,17 +1,16 @@
-import csv
-import codecs
-
+# import csv
+# import codecs
 # def leer_archivo_csv(nombre_archivo):
 #     with codecs.open(nombre_archivo, 'r', encoding='utf-8-sig') as archivo:
 #         next(archivo)  # Saltar la primera línea
 #         lector_csv = csv.DictReader(archivo, skipinitialspace=True)
 #         datos = [fila for fila in lector_csv]
 #     return datos
-
 # def filter_by_specific_values(nombre, columns_to_filter, values_to_filter):
+#     if len(columns_to_filter) != len(values_to_filter):
+#         raise ValueError("Los parámetros columns_to_filter y values_to_filter deben tener la misma longitud")
 #     archivo_csv = f'./data/movies.csv'
 #     datos = leer_archivo_csv(archivo_csv)
-    
 #     resultados_filtrados = []
 #     for fila in datos:
 #         cumple_criterio = True
@@ -23,50 +22,29 @@ import codecs
 #             diccionario_fila = {llave: valor for llave, valor in fila.items()}
 #             resultados_filtrados.append(diccionario_fila)
 #     return resultados_filtrados
-
 # nombre = "data"
 # columns_to_filter = ['genre', 'year']
 # values_to_filter = ['Drama', '1980']
+# try:
+#     resultados = filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
+#     for resultado in resultados:
+#         print(resultado)
+# except ValueError as error:
+#     print(str(error))
 
-# resultados = filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
-# for resultado in resultados:
-#     print(resultado)
-    
-    
-def leer_archivo_csv(nombre_archivo):
-    with codecs.open(nombre_archivo, 'r', encoding='utf-8-sig') as archivo:
-        next(archivo)  # Saltar la primera línea
-        lector_csv = csv.DictReader(archivo, skipinitialspace=True)
-        datos = [fila for fila in lector_csv]
-    return datos
+import pandas as pd
 
-def filter_by_specific_values(nombre, columns_to_filter, values_to_filter):
-    if len(columns_to_filter) != len(values_to_filter):
-        raise Exception("Los parámetros columns_to_filter y values_to_filter deben tener la misma longitud")
-    
-    archivo_csv = f'./data/movies.csv'
-    datos = leer_archivo_csv(archivo_csv)
-    
-    resultados_filtrados = []
-    for fila in datos:
-        cumple_criterio = True
-        for columna, valor in zip(columns_to_filter, values_to_filter):
-            if fila[columna] != valor:
-                cumple_criterio = False
-                break
-        if cumple_criterio:
-            diccionario_fila = {llave: valor for llave, valor in fila.items()}
-            resultados_filtrados.append(diccionario_fila)
-    
-    return resultados_filtrados
+datos = pd.read_csv('./data/movies.csv')
+print(datos)
 
-nombre = "data"
-columns_to_filter = ['genre', 'year']
-values_to_filter = ['Drama', '1980']
 
-try:
-    resultados = filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
-    for resultado in resultados:
-        print(resultado)
-except ValueError as error:
-    print(str(error))
+
+
+    
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import codecs
 
 def leer_archivo_csv(nombre_archivo):
     with codecs.open(nombre_archivo, 'r', encoding='utf-8-sig') as archivo:
-        lector_csv = csv.DictReader(archivo)
-        datos = [fila for fila in lector_csv]
-    return datos
-    
+        lector_csv = csv.reader(archivo, delimiter= ';')
+        
+        
+        header = next(lector_csv)
+        for row in lector_csv:
+            iterable = zip( header, row)
+            
+            name_dict = {key: values for key, values in iterable}
+            print(name_dict)
 
 archivo_csv = './data/movies.csv'
 datos = leer_archivo_csv(archivo_csv)
 
-for fila in datos:
-    diccionario_fila = {llave: valor for llave, valor in fila.items()}
-    print(diccionario_fila)
-    break
+
     
     
     
