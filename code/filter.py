1deac6cb98cb8f4c0dd748714c01579822ee7097
Rotany <Rotany38@gmail.com>
2023-07-11 22:06:49 +0200

    haciendo cambios

--- code/filter.py
+++ code/filter.py
@@ -105,6 +105,7 @@ def export_to_csv(dataframe, columns_to_use, output_file_name):
 #     # Crear un nuevo DataFrame con las columnas seleccionadas
     selected_columns = dataframe[columns_to_use]
     
+    
     # Exportar el DataFrame a un archivo CSV
     selected_columns.to_csv(output_file_name, index=False)
 nombre = './data/movies.csv'
@@ -117,7 +118,8 @@ dataframeblabla = filter_by_specific_values(nombre, columns_to_filter, values_to
 columns_to_use = ['Name', 'Genre', 'Year']
 output_file_name = 'batman.csv'
 
-export_to_csv(dataframeblabla, columns_to_use, output_file_name)
+new_export = export_to_csv(dataframeblabla, columns_to_use, output_file_name)
+print(new_export)
 
 
 
