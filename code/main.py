5990f0502f257b9cc7eb238d7b023e3f49a83bec
Rotany <Rotany38@gmail.com>
2023-07-12 16:20:35 +0200

    haciendo_cambios

--- code/main.py
+++ code/main.py
@@ -1,12 +1,12 @@
-from filter import filter_by_specific_values
 from filter import filter_by_specific_values, export_to_csv
+
 import pandas
 
-#nombre = './data/movies.csv'
-#columns_to_filter = ['Genre', 'Year']
-#values_to_filter = ['Drama', 1981]
-#filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
-#print(filter_by_specific_values(nombre, columns_to_filter, values_to_filter))
+nombre = './data/movies.csv'
+columns_to_filter = ['Genre', 'Year']
+values_to_filter = ['Drama', 1981]
+filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
+print(filter_by_specific_values(nombre, columns_to_filter, values_to_filter))
 
 
 
@@ -19,6 +19,11 @@ values_to_filter = ['Drama', 1981]
 dataframeblabla = filter_by_specific_values(nombre, columns_to_filter, values_to_filter)
 
 columns_to_use = ['Name', 'Genre', 'Year']
-output_file_name = 'batmaqan.csv'
+output_file_name = 'batman.csv'
+print(export_to_csv(dataframeblabla, columns_to_use, output_file_name))
 
-export_to_csv(dataframeblabla, columns_to_use, output_file_name)
+
+
+import pandas as pd
+df = pd.read_csv('Batman.csv')
+print(df)
